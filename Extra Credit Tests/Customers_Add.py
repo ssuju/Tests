7636c92fcc0c75e7e15b9031ae4c7b4d7de75310
Suju Sapkota <ssapkota@unomaha.edu>
2019-11-25 23:19:37 -0600

    finished test cases.

--- Extra Credit Tests/Customers_Add.py
+++ Extra Credit Tests/Customers_Add.py
@@ -11,8 +11,7 @@ import unittest, time, re
 class CustomersAdd(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Firefox()
-        self.driver.implicitly_wait(30)
-        self.base_url = "https://www.katalon.com/"
+        self.driver.implicitly_wait(10)
         self.verificationErrors = []
         self.accept_next_alert = True
 
